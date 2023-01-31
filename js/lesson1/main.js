abf50b52c315c967fa5d888e2628ed0c4ef8d875
WinterSakuraa <darwinwoda@gmail.com>
2023-02-01 01:03:50 +0200

    js -  lesson 1

---
+++ js/lesson1/main.js
@@ -0,0 +1,62 @@
+// TASK 1
+const hello = 'hello';
+console.log(hello);
+
+const owu = 'owu';
+console.log(owu);
+
+const com = 'com';
+console.log(com);
+
+const ua = 'ua';
+console.log(ua);
+
+const num1 = 1;
+console.log(num1);
+
+const num2 = 10;
+console.log(num2);
+
+const num3 = -999;
+console.log(num3);
+
+const num4 = 123;
+console.log(num4);
+
+const num5 = 3.14;
+console.log(num5);
+
+const num6 = 2.7;
+console.log(num6);
+
+const num7 = 16;
+console.log(num7);
+
+const boolTrue = true;
+console.log(boolTrue);
+
+const boolFalse = false;
+console.log(boolFalse);
+
+// TASK 2
+const firstName = 'olya';
+const lastName = 'tkhay';
+let middleName = 'oleksiivna';
+const person = `${ lastName } ${ firstName } ${ middleName }`;
+console.log(person);
+
+// TASK 3
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
+
+// TASK 4
+const name = prompt('What is your name?', '');
+const surname = prompt('What is your surname?', '');
+const age = prompt('What is your age?', '');
+console.log(`Hello ${ name } ${ surname }, you are ${ age } years old`);
