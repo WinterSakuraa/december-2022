c5872002b916aef8f376a7d54d3d74a414bc463a
WinterSakuraa <darwinwoda@gmail.com>
2023-02-11 15:32:05 +0200

    js -  lesson 4

---
+++ js/lesson4/main.js
@@ -0,0 +1,130 @@
+// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+function rectangleArea(a, b) {
+    return a * b;
+}
+
+// - створити функцію яка обчислює та повертає площу кола з радіусом r
+function circleArea(r) {
+    return Math.PI * Math.pow(r, 2);
+}
+
+// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+function cylinderArea(r, h) {
+    return 2 * Math.PI * r * (h + r);
+}
+
+// - створити функцію яка приймає масив та виводить кожен його елемент
+function printArray(arr) {
+    for (const elem of arr) {
+        console.log(elem);
+    }
+}
+
+// - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+function createP(text) {
+    document.write(`<p>${ text }</p>`);
+}
+
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+function createUl(liText) {
+    document.write(`
+        <ul>
+            <li>${ liText }</li>
+            <li>${ liText }</li>
+            <li>${ liText }</li>
+        </ul>
+`);
+}
+
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+function createUlByLoop(liText, liAmount) {
+    document.write(`<ul>`);
+    for (let i = 0; i < liAmount; i++) {
+        document.write(`<li>${ liText }</li>`);
+    }
+    document.write(`</ul>`);
+}
+
+// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+function createList(arr) {
+    document.write(`<ul>`);
+    for (const elem of arr) {
+        document.write(`<li>${ elem } of type ${ typeof elem }</li>`);
+    }
+    document.write(`</ul>`);
+}
+
+// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+let objArray = [
+    {
+        id: 1,
+        name: 'Biba',
+        age: 20,
+    },
+    {
+        id: 2,
+        name: 'Boba',
+        age: 25,
+    },
+    {
+        id: 3,
+        name: 'Buba',
+        age: 18,
+    },
+];
+
+function printObjectArray(arr) {
+    for (const object of arr) {
+        document.write(`<div>
+                            <p>id: ${ object.id }</p>
+                            <p>name: ${ object.name }</p>
+                            <p>age: ${ object.age }</p>
+                        </div>`);
+    }
+}
+
+// - створити функцію яка повертає найменьше число з масиву
+function findMinValue(arr) {
+    let min = arr[0];
+
+    for (const elem of arr) {
+        if (elem < min) {
+            min = elem;
+        }
+    }
+
+    return min;
+}
+
+// - створити функцію sum(arr) яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
+function sum(arr) {
+    let sum = 0;
+
+    for (const elem of arr) {
+        sum += elem;
+    }
+
+    return sum;
+}
+
+// - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
+// Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+function swap(arr, index1, index2) {
+    let temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+
+// - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+// Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
+function exchange(sumUAH, currencyValues, exchangeCurrency) {
+    let currency = null;
+
+    for (const elem of currencyValues) {
+        if (elem.currency === exchangeCurrency) {
+            currency = elem;
+        }
+    }
+
+    return sumUAH / currency.value;
+}
