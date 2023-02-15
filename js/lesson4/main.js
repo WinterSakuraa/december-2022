8ea469bbf04549a160f48f9df94177090f9ab2af
WinterSakuraa <darwinwoda@gmail.com>
2023-02-15 14:46:57 +0200

    js -  lesson 5

--- js/lesson4/main.js
+++ js/lesson4/main.js
@@ -1,32 +1,33 @@
 // - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
-function rectangleArea(a, b) {
+rectangleArea = (a, b) => {
     return a * b;
-}
+};
 
 // - створити функцію яка обчислює та повертає площу кола з радіусом r
-function circleArea(r) {
+circleArea = (r) => {
     return Math.PI * Math.pow(r, 2);
-}
+};
 
 // - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
-function cylinderArea(r, h) {
+cylinderArea = (r, h) => {
     return 2 * Math.PI * r * (h + r);
-}
+};
 
 // - створити функцію яка приймає масив та виводить кожен його елемент
-function printArray(arr) {
+printArray = (arr) => {
     for (const elem of arr) {
         console.log(elem);
     }
-}
+};
 
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
-function createP(text) {
+createP = (text);
+{
     document.write(`<p>${ text }</p>`);
 }
 
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-function createUl(liText) {
+createUl = (liText) => {
     document.write(`
         <ul>
             <li>${ liText }</li>
@@ -34,25 +35,25 @@ function createUl(liText) {
             <li>${ liText }</li>
         </ul>
 `);
-}
+};
 
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
-function createUlByLoop(liText, liAmount) {
+createUlByLoop = (liText, liAmount) => {
     document.write(`<ul>`);
     for (let i = 0; i < liAmount; i++) {
         document.write(`<li>${ liText }</li>`);
     }
     document.write(`</ul>`);
-}
+};
 
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
-function createList(arr) {
+createList = (arr) => {
     document.write(`<ul>`);
     for (const elem of arr) {
         document.write(`<li>${ elem } of type ${ typeof elem }</li>`);
     }
     document.write(`</ul>`);
-}
+};
 
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
 let objArray = [
@@ -73,7 +74,7 @@ let objArray = [
     },
 ];
 
-function printObjectArray(arr) {
+printObjectArray = (arr) => {
     for (const object of arr) {
         document.write(`<div>
                             <p>id: ${ object.id }</p>
@@ -81,10 +82,10 @@ function printObjectArray(arr) {
                             <p>age: ${ object.age }</p>
                         </div>`);
     }
-}
+};
 
 // - створити функцію яка повертає найменьше число з масиву
-function findMinValue(arr) {
+findMinValue = (arr) => {
     let min = arr[0];
 
     for (const elem of arr) {
@@ -94,10 +95,10 @@ function findMinValue(arr) {
     }
 
     return min;
-}
+};
 
 // - створити функцію sum(arr) яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
-function sum(arr) {
+sum = (arr) => {
     let sum = 0;
 
     for (const elem of arr) {
@@ -105,19 +106,19 @@ function sum(arr) {
     }
 
     return sum;
-}
+};
 
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
-function swap(arr, index1, index2) {
+swap = (arr, index1, index2) => {
     let temp = arr[index1];
     arr[index1] = arr[index2];
     arr[index2] = temp;
-}
+};
 
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
-function exchange(sumUAH, currencyValues, exchangeCurrency) {
+exchange = (sumUAH, currencyValues, exchangeCurrency) => {
     let currency = null;
 
     for (const elem of currencyValues) {
@@ -127,4 +128,4 @@ function exchange(sumUAH, currencyValues, exchangeCurrency) {
     }
 
     return sumUAH / currency.value;
-}
+};
